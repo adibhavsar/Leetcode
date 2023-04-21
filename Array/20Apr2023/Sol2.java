dbc6f5d835baf0ec80a481e6d43a724bdd05efe2
adibhavsar <103627203+adibhavsar@users.noreply.github.com>
2023-04-21 15:53:58 +0530

    20/04/2023

---
+++ Array/20Apr2023/Sol2.java
@@ -0,0 +1,21 @@
+//my solution 
+
+class Solution {
+    public int maximumWealth(int[][] accounts) {
+        int max = 0;
+        int temp = 0;
+
+        for (int i = 0; i < accounts.length; i++) {
+            for (int j = 0; j < accounts[i].length; j++) {
+                temp += accounts[i][j];
+            }
+
+            if (temp > max) {
+                max = temp;
+            }
+            temp = 0;
+        }
+
+        return max;
+    }
+}
