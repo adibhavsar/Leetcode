6dec8f7de1481a59222991659caca8f0b90a8f44
adibhavsar <103627203+adibhavsar@users.noreply.github.com>
2023-05-07 23:19:32 +0530

    07/05/2023

---
+++ String/07May2023/Sol1.java
@@ -0,0 +1,13 @@
+//my solution 
+
+class Solution {
+    public String interpret(String command) {
+        if (command.contains("()")) {
+            command = command.replace("()", "o");
+        }
+        if (command.contains("(al)")) {
+            command = command.replace("(al)", "al");
+        }
+        return command;
+    }
+}
